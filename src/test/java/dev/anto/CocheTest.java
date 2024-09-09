a7ab4184fb4dbb2f5b4bac1470d3dda8b1a57378
Antonella <168837149+AntonellaEL@users.noreply.github.com>
2024-09-09 18:51:13 +0200

    coche test

---
+++ src/test/java/dev/anto/CocheTest.java
@@ -0,0 +1,16 @@
+package dev.anto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CocheTest {
+
+    @Test
+    public void testValorPeajeCoche(){
+        Coche coche = new Coche("ABC349");
+        int valorPeaje = coche.getValorPeaje();
+    assertEquals(100, valorPeaje);   
+}
+    
+}
