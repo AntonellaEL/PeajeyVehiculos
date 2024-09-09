59bf917cacce4ffa0ed7cf5f8db69597589e7008
Antonella <168837149+AntonellaEL@users.noreply.github.com>
2024-09-09 18:55:32 +0200

    camion test

---
+++ src/test/java/dev/anto/CamionTest.java
@@ -0,0 +1,14 @@
+package dev.anto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CamionTest {
+    @Test
+    public void testValorPeajeCamion() {
+        Camion camion = new Camion("AGU856", 6);
+        int valorPeaje = camion.getValorPeaje();
+        assertEquals(300, valorPeaje); 
+    }
+}
