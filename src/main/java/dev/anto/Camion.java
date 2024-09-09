ff49b0ef98e89ed6fa35c64d4476ab8288bfc57c
Antonella <168837149+AntonellaEL@users.noreply.github.com>
2024-09-09 14:32:52 +0200

    class camion

---
+++ src/main/java/dev/anto/Camion.java
@@ -0,0 +1,16 @@
+package dev.anto;
+
+public class Camion extends Vehiculo {
+    
+    private int numeroEjes;
+    private int valorPeajePorEje = 50;
+
+    Camion(String placa, int numeroEjes) {
+        super(placa);
+        this.numeroEjes = numeroEjes;
+    }
+
+    public int getValorPeaje() {
+        return numeroEjes * valorPeajePorEje;
+    }
+}
