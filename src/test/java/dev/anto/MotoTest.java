778c458aca79b557632ab4a31c7d10287996567f
Antonella <168837149+AntonellaEL@users.noreply.github.com>
2024-09-09 18:52:35 +0200

    moto test

---
+++ src/test/java/dev/anto/MotoTest.java
@@ -0,0 +1,14 @@
+package dev.anto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MotoTest {
+        @Test
+    public void testValorPeajeCoche(){
+        Moto moto = new Moto("ANT666");
+        int valorPeaje = moto.getValorPeaje();
+    assertEquals(50, valorPeaje);   
+}
+}
